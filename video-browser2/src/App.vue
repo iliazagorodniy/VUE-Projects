422c283a1e17ebead46ce1b6d29787513509c5b9
Ilia <izagorodn@gmail.com>
2019-11-10 01:50:19 +0300

    Add files via upload

---
+++ video-browser2/src/App.vue
@@ -0,0 +1,53 @@
+<template>
+    <div class="container">
+        <SearchBar @termChange="onTermChange"></SearchBar>
+        <div class="row">
+            <VideoDetail :video="selectedVideo"></VideoDetail>
+            <VideoList @videoSelect="onVideoSelect" :videos="videos"></VideoList>
+        </div>
+    </div>
+</template>
+
+<script>
+    import axios from 'axios';
+    import SearchBar from "./components/SearchBar";
+    import VideoList from "./components/VideoList";
+    import VideoDetail from "./components/VideoDetail";
+    const API_KEY ='AIzaSyBFOIKlsoSUPWT1P94scug92meQ-R3IhbQ';
+
+    export default {
+        name: 'App',
+        components: {
+            VideoDetail,
+            VideoList,
+            SearchBar
+        },
+        data() {
+            return {
+                videos:[],
+                selectedVideo: null,
+            };
+        },
+        methods: {
+            onVideoSelect(video) {
+                this.selectedVideo = video;
+            },
+            onTermChange(searchTerm) {
+                axios.get('https://www.googleapis.com/youtube/v3/search', {
+                    params: {
+                        key: API_KEY,
+                        type: 'video',
+                        part: 'snippet',
+                        q: searchTerm
+                    }
+                }).then(response => {
+                    this.videos = response.data.items
+                });
+            },
+        }
+    };
+</script>
+
+<style scoped>
+
+</style>
